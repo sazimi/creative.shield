def4993ff736460b3aa9962aaec26c44380baba7
NimaZahedi <Nima.Zahedee@gmail.com>
2015-04-29 18:26:34 +0200

    Fixed Error status

--- app/modules/backend/states.ts
+++ app/modules/backend/states.ts
@@ -63,29 +63,49 @@ module backend{
     .state('main.shield.backend.travis', {
         url: '/travis',
         template: 'Hi, This is {{name}}! <br /> <img src="demo/images/travis.png" />',
-        controller: function($scope, travisServices) {
+        controller: function($scope, $rootScope, travisServices) {
             $scope.name = travisServices.getName();
+            $rootScope.hasError = false;
+            $rootScope.stateName = "main.shield.backend.travis";
+            $rootScope.status = !$rootScope.hasError;
+            $rootScope.moduleName = "backend";
+            $rootScope.url = "/backend/travis"
         }
     })
     .state('main.shield.backend.kim', {
         url: '/kim',
         template: 'Hi, This is {{name}}! <br /> <img src="demo/images/kim.png" />',
-        controller: function($scope, kimServices) {
+        controller: function($scope, $rootScope, kimServices) {
             $scope.name = kimServices.getName();
+            $rootScope.hasError = false;
+            $rootScope.stateName = "main.shield.backend.kim";
+            $rootScope.status = !$rootScope.hasError;
+            $rootScope.moduleName = "backend";
+            $rootScope.url = "/backend/kim"
         }
     })
     .state('main.shield.backend.hristo', {
         url: '/hristo',
         template: 'Hi, This is {{name}}! <br /> <img src="demo/images/hristo.png" />',
-        controller: function($scope, hristoServices) {
+        controller: function($scope, $rootScope, hristoServices) {
             $scope.name = hristoServices.getName();
+            $rootScope.hasError = true;
+            $rootScope.stateName = "main.shield.backend.hristo";
+            $rootScope.status = !$rootScope.hasError;
+            $rootScope.moduleName = "backend";
+            $rootScope.url = "/backend/hristo"
         }
     })
     .state('main.shield.backend.sebastian', {
         url: '/sebastian',
         template: 'Hi, This is {{name}}! <br /> <img src="demo/images/sebastian.png" />',
-        controller: function($scope, sebastianServices) {
+        controller: function($scope, $rootScope, sebastianServices) {
             $scope.name = sebastianServices.getName();
+            $rootScope.hasError = false;
+            $rootScope.stateName = "main.shield.backend.sebastian";
+            $rootScope.status = !$rootScope.hasError;
+            $rootScope.moduleName = "backend";
+            $rootScope.url = "/backend/sebastian"
         }
     })
     ;
